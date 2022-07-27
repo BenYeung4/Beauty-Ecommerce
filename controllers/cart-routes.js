7263aa05bbffcceced335ac02884c128c6a96cf8
chrispobrien <chris.obrien57@gmail.com>
2022-07-27 09:29:36 -0400

    Cart work - still working

--- controllers/cart-routes.js
+++ controllers/cart-routes.js
@@ -10,13 +10,22 @@ router.get('/', (req, res) => {
             user_id: req.session.user_id,
         },
         include: Product,
+        attributes: { include: [['(quantity*product.price)', 'subtotal']] }
     })
         .then((dbCartData) => {
+            // Add subtotal for each item
+            // dbCartData = dbCartData.map((item) => item.subtotal = item.quantity*item.product.price);
             // Serialize each product //
             const cart = dbCartData.map((item) => item.get({ plain: true }));
+            // Calculate total in cart
+            const total = dbCartData.reduce((tot, item) => tot += item.quantity*item.product.price, 0).toLocaleString('en-US', { style: 'currency', currency: 'USD'});
+            // Calculate total number of items in cart
+            const totalitems = dbCartData.reduce((tot, item) => tot += item.quantity, 0);
             // Render //
             res.render('cart', {
                 cart,
+                total,
+                totalitems,
                 loggedIn: req.session.loggedIn,
                 isAdmin: req.session.isAdmin,
             });
