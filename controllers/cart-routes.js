ba8e2bb2f9bf2c07a61b64ecdc932d7a200f0f9d
Damaris Canales <damariscanales00@gmail.com>
2022-07-27 10:46:03 -0400

    Top Bar Edits

--- controllers/cart-routes.js
+++ controllers/cart-routes.js
@@ -3,20 +3,39 @@ const sequelize = require('../config/connection');
 const { User, Product, Cart, allProducts } = require('../models');
 
 router.get('/', (req, res) => {
-    Cart.findAll()
-    .then(dbProductData => {
-        // Serialize each product //
-        const products = dbProductData.map(product => product.get({ plain: true }));
-        // Render //
-        res.render('cart', {
-            products,
-            loggedIn: req.session.loggedIn
+    Cart.findAll({
+        where: {
+            user_id: req.session.user_id,
+        },
+        include: Product,
+        attributes: { include: [['(quantity*product.price)', 'subtotal']] }
+    })
+        .then((dbCartData) => {
+            // Add subtotal for each item
+            // dbCartData = dbCartData.map((item) => item.subtotal = item.quantity*item.product.price);
+            // Serialize each product //
+            const cart = dbCartData.map((item) => item.get({ plain: true }));
+            // Calculate total in cart
+            const total = dbCartData.reduce((tot, item) => tot += item.quantity*item.product.price, 0).toLocaleString('en-US', { style: 'currency', currency: 'USD'});
+            // Calculate total number of items in cart
+            const totalitems = dbCartData.reduce((tot, item) => tot += item.quantity, 0);
+            // Render //
+            res.render('cart', {
+                cart,
+                total,
+                totalitems,
+                loggedIn: req.session.loggedIn,
+                isAdmin: req.session.isAdmin,
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
         });
     })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
     })
-})
 
 module.exports = router;
